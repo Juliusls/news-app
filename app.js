0f777247aaeb3abcdbcb51c3d3860ef41f36750a
Julius <julius.lubys@gmail.com>
2021-03-14 15:11:51 +0200

    Fixing and issua of cookies not being stored in browser 15

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 		logger.error('error connecing to MongoDB', error.message)
 	})
 
-app.use(cors({origin: 'https://julius-news-app.netlify.app'}))
+app.use(cors())
 app.use(express.static('build'))
 app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }))
@@ -60,23 +60,6 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 app.use(middleware.errorHandler)
-app.use(function (req, res, next) {
-
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', 'https://julius-news-app.netlify.app/')
-
-	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-
-	// Set to true if you need the website to include cookies in the requests sent
-	// to the API (e.g. in case you use sessions)
-	res.setHeader('Access-Control-Allow-Credentials', true)
-
-	// Pass to next layer of middleware
-	next()
-})
+app.use(middleware.cookieChecker)
 
 module.exports = app
